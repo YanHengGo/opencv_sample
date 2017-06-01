4a84abf934efe592d1093c952e27033f838ef226
YanHengGo <dang712@hotmail.com>
2017-06-01 10:33:17 +0900

    image to mat module

---
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+
+int main()
+{
+	Mat srcImage = imread("1.jpg");
+	imshow("org1", srcImage);
+	Mat element = getStructuringElement(MORPH_RECT, Size(15, 15));
+	Mat dstImage;
+	erode(srcImage, dstImage, element);
+	imshow("org2", dstImage);
+	waitKey(0);
+
+	return 0;
+}
